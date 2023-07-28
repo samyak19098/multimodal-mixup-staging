25c8f4ca0330d43697927caf78f1b86da189456c
Samyak Jain <samyak19098@iiitd.ac.in>
2023-07-28 21:35:11 +0530

    fix

--- m3a/m3a_ssmix_optuna.py
+++ m3a/m3a_ssmix_optuna.py
@@ -677,7 +677,7 @@ def objective(trial):
 		# "threshold": trial.suggest_loguniform("threshold", 0.1, 0.8)
 		# "lam_inter": trial.suggest_loguniform("lam_inter", 0.1, 0.8)
 		"learning_rate": trial.suggest_loguniform("lr", 6e-4, 2e-3),
-		"loss_original_coef": trial.suggest_loguniform("loss_original_coef", 0.1, 0),
+		"loss_original_coef": trial.suggest_loguniform("loss_original_coef", 0.1, 1),
 		"loss_intra_coef": trial.suggest_loguniform("loss_intra_coef", 0.1, 1),
 		"loss_inter_coef": trial.suggest_loguniform("loss_inter_coef", 0.1, 1),
 		"lam_inter": trial.suggest_loguniform("lam_inter", 0.2, 0.6),
