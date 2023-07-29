49a0c7461bdec9a6ae75ba259098bb869ea52593
Parth Chhabra <parth19069@iiitd.ac.in>
2023-07-29 20:14:11 +0530

    Fix

--- m3a/m3a_ssmix_optuna.py
+++ m3a/m3a_ssmix_optuna.py
@@ -8,8 +8,6 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.layers import *
 import sys
 
-from custom_models import *
-
 from tqdm.auto import tqdm
 from sklearn.metrics import *
 import pandas as pd
@@ -87,7 +85,37 @@ movement_dropout = 0.0
 
 """# M3A Function Declaration"""
 
+def createMdrmLstm(maxlen, input_shape, output_shape, last_layer_activation, flatten=False):
+    input_data = Input(shape=(maxlen, input_shape))
+    masked = Masking(mask_value =0)(input_data)
+    lstm = Bidirectional(LSTM(300, activation='tanh', return_sequences = True, dropout=0.6))(masked)
+    inter = Dropout(0.9)(lstm)
+    if flatten:
+        flattened = Flatten()(inter)
+        inter1 = Dense(output_shape, activation=last_layer_activation)(flattened)
+    else:
+        inter1 = TimeDistributed(Dense(output_shape,activation=last_layer_activation))(inter)
+    model = Model(input_data, inter1)
 
+    return model
+
+def createMdrm(maxlen, audio_shape, text_shape):
+    audio = Input(shape=(maxlen, audio_shape))
+    text = Input(shape=(maxlen, text_shape))
+
+    lstm_audio = createMdrmLstm(maxlen, audio_shape, 100, 'tanh')
+    lstm_text = createMdrmLstm(maxlen, text_shape, 100, 'tanh')
+    concatenator = Concatenate(axis=2)
+    lstm_combined = createMdrmLstm(maxlen, 200, 1, 'sigmoid', flatten=True)
+
+    audio_features = lstm_audio(audio)
+    text_features = lstm_text(text)
+    combined_features = concatenator([audio, text])
+    logits = lstm_combined(combined_features)
+
+    model = Model(inputs=[audio, text], outputs=[logits])
+
+    return model
 class MultiHeadSelfAttention(layers.Layer):
 		def __init__(self, embed_dim, num_heads=8, **kwargs):
 				super(MultiHeadSelfAttention, self).__init__(**kwargs)
